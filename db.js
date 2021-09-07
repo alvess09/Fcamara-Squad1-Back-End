d1ef378c041905f3cd6988ea50551e7ecff24a0e
Ariel Alves <arielalves9@gmail.com>
2021-09-07 18:43:15 -0300

    alteração nos diretórios

---
+++ db.js
@@ -0,0 +1,30 @@
+module.exports = {
+    dbFunc,
+    dbDesk
+}
+/*Construtor de dados dos Usuarios*/
+const data = {
+    nome: String,
+    email: String,
+    cidade: String
+}
+const dbFunc = insereUsuarios [] = [] 
+
+/* A ideia é que os dados fiquem populados aqui por enquanto para depois criarmos as rotas do back-end */
+const insereUsuarios= ()=>{
+    dbFunc.push({
+        nome: data.nome,
+        email: data.email,
+        cidade: data.cidade
+    });
+}
+
+const disponiveis = []
+    for (let i = 0; i < 600; i++) {
+        if (i % 4 == 0){
+            disponiveis.push(i)
+        }
+    }
+    console.log
+const dbDesk = string.reduce( x => x % 4 == 0)
+console.log(selector3)
