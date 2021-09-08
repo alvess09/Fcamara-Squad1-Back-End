cbce0fa723f3a8ac4b90d76a43cdb0e71ca13617
Ariel Alves <arielalves9@gmail.com>
2021-09-07 23:43:34 -0300

    organizando rotas

--- Index.js
+++ Index.js
@@ -1,14 +1,41 @@
 /* Criando um Servidor c/ express*/
 const express = require('express');
+const morgan = require("morgan");
 const app = express();
 
+app.use(morgan('dev'));
+
 //Rotas de acesso
+
 app.get('/', (req, res) =>{
     //regra de negocio entra aqui
-    res.json({ mensagem:'Rota Funcionando'});
+    res.json({ mensagem:'PEGAR TODOS OS REGISTROS'});
 });
+// PEGAR SOMENTE REGISTRO COM ID
+app.get('/:id',(req, res) =>{
+    res.json({ mensagem:`PEGAR SOMENTE OS REGISTROS COM ID:${id} `});
+});
+// CRIAR UM REGISTRO 
+app.post('/',(req, res) =>{
+    const body = req.body
+    res.json(body);
+});
+
+// ATUALIZAR SOMENTE REGISTRO COM ID
+app.put('./:id', (req, res) => {
+    res.json({mensagem: `ATUALIZAR SOMENTE O REGISTRO COM ID: ${id}`});
+});
+
+// DELETAR SOMENTE REGISTRO COM ID
+app.delete('./:id', (req, res) => {
+    const id = req.params.id;
+    res.json({ mensagem: `DELETAR SOMENTE O REGISTRO COM O ID: ${id}`});
+});
+
+
+
 
 // Inicialização do Servidor
 app.listen(3000, () =>{
     console.log("Meu Servidor está funcionando!")
-})
+});
