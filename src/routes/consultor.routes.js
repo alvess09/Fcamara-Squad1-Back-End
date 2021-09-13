a5c2ae406f8c30bbea046c723bdc03094a1abd82
lepitsch <72175601+lepitsch@users.noreply.github.com>
2021-09-13 13:46:39 -0300

    recuperando registro pelo ID rota consultor

--- src/routes/consultor.routes.js
+++ src/routes/consultor.routes.js
@@ -17,9 +17,15 @@ router.get('/', async (req, res) =>{
 });
 
 // PEGAR SOMENTE REGISTRO COM ID
-router.get('/:id',(req, res) =>{
-    const id = req.params.id;
-    res.json({ mensagem:`PEGAR SOMENTE OS REGISTROS COM ID:${id} `});
+router.get('/:id', async (req, res) =>{
+    try {
+        const id = req.params.id;
+        const consultor = await Consultor.findByID(id);
+        res.json({ error: false, consultor })
+    } catch (err) {
+        res.json({ error: true, message: err.message });
+    }
+
 });
 
 // CRIAR UM REGISTRO 
